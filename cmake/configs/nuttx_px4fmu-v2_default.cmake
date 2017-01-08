4e2b780dc554f11693c9eb99fc9ffedc5517af42
James Goppert <james.goppert@gmail.com>
2017-01-07 20:07:36 -0500

    Changes to make lpe default.

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -103,8 +103,8 @@ set(config_module_list
 	#
 	#modules/attitude_estimator_q
 	#modules/position_estimator_inav
-	#modules/local_position_estimator
-	modules/ekf2
+	modules/local_position_estimator
+	#modules/ekf2
 
 	#
 	# Vehicle Control
